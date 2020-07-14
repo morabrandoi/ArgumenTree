7225cda0f6f20b1d0aaf94f9d23f993464cae7a2
Brando Mora <brandomora@fb.com>
2020-07-14 10:54:44 -0700

    Added Firebase Firestore and Authentication to project dependencies. Also added fragment and activity skeleton

--- app/src/main/java/com/example/argumentree/MainActivity.java
+++ app/src/main/java/com/example/argumentree/MainActivity.java
@@ -1,14 +1,58 @@
 package com.example.argumentree;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment = null;
+                switch (menuItem.getItemId()) {
+                    case R.id.action_profile:
+                        // TODO: fill switch with Profile Fragment only if signed in. Otherwise go to Log in activity
+//                        fragment = new ProfileFragment();
+                        break;
+                    case R.id.action_home:
+                        // TODO: fill switch with Home Fragment
+//                        fragment = new HomeFragment();
+                        break;
+                    case R.id.action_compose:
+                        // TODO: fill switch with ComposeActivityIntent
+
+                        break;
+                    case R.id.action_notifications:
+                        // TODO: fill switch with Notifications Fragment
+//                        fragment = new NotificationsFragment();
+                    case R.id.action_search:
+                        // TODO: fill switch with Search Fragment
+//                        fragment = new SearchFragment();
+                    default:
+                        fragment = null;
+                }
+
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
     }
 }
---
+++ app/src/main/java/com/example/argumentree/NodeDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.argumentree;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NodeDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_node_detail);
+    }
+}
