16fff132e72e751870bd8ab0eff42b8e4247e167
Brando Mora <morabrandoi@gmail.com>
2020-07-17 08:57:42 -0700

    Added Constant.java

---
+++ app/src/main/java/com/example/argumentree/Constants.java
@@ -0,0 +1,32 @@
+package com.example.argumentree;
+
+public class Constants {
+    // For users document
+    public static final String KEY_USER_EMAIL = "email";
+    public static final String KEY_USER_USERNAME = "username";
+    public static final String KEY_USER_PROFILE_PIC = "profile_pic";
+    public static final String KEY_USER_BIO = "bio";
+    public static final String KEY_USER_AUTH_USER_ID = "auth_user_id";
+    public static final String KEY_USER_LIKES = "likes";
+    public static final String KEY_USER_CREATED_AT = "createdAt";
+
+    // for question post document
+    public static final String KEY_QUESTION_BODY = "body";
+    public static final String KEY_QUESTION_TAGS = "tags";
+    public static final String KEY_QUESTION_AUTHOR = "author";
+    public static final String KEY_QUESTION_MEDIA = "media";
+    public static final String KEY_QUESTION_DESCENDANTS = "descendants";
+    public static final String KEY_QUESTION_CREATED_AT = "createdAt";
+
+    // for response post document
+    public static final String KEY_RESPONSE_PARENT = "parent";
+    public static final String KEY_RESPONSE_QUESTION_ROOT = "question_root";
+    public static final String KEY_RESPONSE_DESCENDANTS = "descendants";
+    public static final String KEY_RESPONSE_AGREEMENTS = "agreements";
+    public static final String KEY_RESPONSE_DISAGREEMENTS = "disagreements";
+    public static final String KEY_RESPONSE_TITLE = "title";
+    public static final String KEY_RESPONSE_CLAIM = "claim";
+    public static final String KEY_RESPONSE_SOURCE = "source";
+    public static final String KEY_RESPONSE_SOURCE_QUESTION = "source_question";
+    public static final String KEY_RESPONSE_CREATED_AT = "createdAt";
+}
